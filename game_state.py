4e3439da54bd6816f76d07e0590293b49a6ab49c
Thomas Magnusson <thomas.magnusson1@marist.edu>
2020-05-22 16:29:13 -0400

    fixes select_card function to avoid being player specific

--- game_state.py
+++ game_state.py
@@ -4,9 +4,10 @@ from deck import Deck
 from player import Player
 from queue import Queue
 
+
 class GameState:
 
-    def __init__(self, inputFn = input):
+    def __init__(self, inputFn=input):
         self.player1 = Player()
         self.player2 = Player()
         self.deck = Deck.shuffled()
@@ -14,7 +15,8 @@ class GameState:
         self.center_card = None
         self.played_stack = []
         self.crib = []
-        self.phases = [self.re_shuffle, self.deal, self.make_crib, self.cut, self.start]
+        self.phases = [self.re_shuffle, self.deal,
+                       self.make_crib, self.cut, self.start]
         self.dealer = self.player1.id
         self.input = inputFn
 
@@ -36,30 +38,33 @@ class GameState:
 
     def prompt_crib(self, player):
         print("Lay away...")
-        self.crib.append(self.select_card(player)) 
+        self.crib.append(self.select_card(player.hand))
         print("Lay away...")
-        self.crib.append(self.select_card(player))
-    
-    def select_card(self, player):
+        self.crib.append(self.select_card(player.hand))
+
+    def select_card(self, cards):
         """
         Prompts the player to select a card from their hand
         Returns the selected card, which is removed from their hand.
         """
+        idx = -1  # should not be inital value
         while True:
             print("Please select a card by index:")
-            inpt = self.input(list(enumerate(player.hand)))
+            inpt = self.input(list(enumerate(cards)))
             try:
-               idx = int(inpt) 
+                idx = int(inpt)
             except ValueError:
                 print(f"'{inpt}' is not a valid index.")
-            if idx < 0 or idx >= len(player.hand):
+            if idx < 0 or idx >= len(cards):
                 print(f"The index {idx} is not available.")
             else:
                 break
-        return player.hand.pop(idx)
+        assert idx != -1  # make sure it's not initial value
+        return cards.pop(idx)
 
     def make_crib(self):
-        print('Cards should now be layed away into the crib. Please pass the game to player 1')
+        print(
+            'Cards should now be layed away into the crib. Please pass the game to player 1')
         print('Player 1:')
         self.prompt_crib(self.player1)
 
@@ -70,7 +75,8 @@ class GameState:
         print('The crib has been created.')
 
     def cut(self):
-        cut_number = int(self.input(f'Player {self.player1.id}, please select a number between 5 and {len(self.deck)} to cut the deck'))
+        cut_number = int(self.input(
+            f'Player {self.player1.id}, please select a number between 5 and {len(self.deck)} to cut the deck'))
         self.center_card = self.deck[cut_number]
         print(f'The center card is {self.center_card}')
 
@@ -85,18 +91,21 @@ class GameState:
             print(f"the count is {the_count}")
             current_player = player_queue.pop(0)
             print(current_player)
-            playable_cards = list(self.filter_playable_cards(current_player.hand, the_count))
+            playable_cards = list(self.filter_playable_cards(
+                current_player.hand, the_count))
             if len(playable_cards) == 0:
                 print("GO")
                 # TODO: the other player gets a point
                 break
             print(f"Play any of these: {playable_cards}")
             # TODO: sanitize input from user
-            card_played = current_player.hand.pop(int(self.input("Play a card from your hand (enter the index of the card - first card is 0)")))
+            card_played = current_player.hand.pop(int(self.input(
+                "Play a card from your hand (enter the index of the card - first card is 0)")))
             self.played_stack.append(card_played)
             print(f"{card_played} was played for {card_played.rank.points()} points.")
             the_count += card_played.rank.points()
-            current_player.score += self.apply_score(self.played_stack, the_count)
+            current_player.score += self.apply_score(
+                self.played_stack, the_count)
             player_queue.append(current_player)
 
     def filter_playable_cards(self, hand, count):
@@ -107,10 +116,11 @@ class GameState:
 
     straight = []
     matches = []
+
     def apply_score(self, played_stack, count):
         score = 0
         if count == 15 or count == 31:
-            # Count is now 31 -> 2 points 
+            # Count is now 31 -> 2 points
             # Count is now 15 -> 2 points
             score += 2
         # Previous N are unordered straight (at least 3, three -> 3 points, four -> 4 points, etc)
@@ -125,12 +135,13 @@ class GameState:
             if abs(diff) == 1:
                 # it's potentially a straight... keep the straight queue going
                 self.straight.append(played_stack[-1])
-                self.straight.sort() # for easier checking
+                self.straight.sort()  # for easier checking
             else:
                 # no straight, reset the queue
                 self.straight.clear()
-                self.straight.append(played_stack[-1]) # because it could still be this card in it, just reset the rest though
-        else: # straight queue has at least 2 cards in it
+                # because it could still be this card in it, just reset the rest though
+                self.straight.append(played_stack[-1])
+        else:  # straight queue has at least 2 cards in it
             # check the bottom and top completion next
             bot = self.straight[0]
             top = self.straight[-1]
@@ -141,20 +152,22 @@ class GameState:
                 # add to the queue
                 self.straight.append(cmp)
                 # add points for the number of cards in the straight queue
-                score += straight_queue_len + 1 # +1 bc of extra card added
+                score += straight_queue_len + 1  # +1 bc of extra card added
 
         # Previous N are same rank (pair -> 2 points, triplet -> 6 points, quadruplet -> 12)
         matches_len = len(self.matches)
         if matches_len == 0:
-            self.matches.append(played_stack[-1]) # add the last
-        else: # matches has at least one card, lets see if rank matches
+            self.matches.append(played_stack[-1])  # add the last
+        else:  # matches has at least one card, lets see if rank matches
             cmp = self.matches[0]
             if cmp.rank == played_stack[-1].rank:
                 # add to the matches, add the points
                 self.matches.append(played_stack[-1])
-                score += matches_len + matches_len**2 # a[n] = n^2 + n is [2,6,12...]
+                # a[n] = n^2 + n is [2,6,12...]
+                score += matches_len + matches_len**2
             else:
                 self.matches.clear()
-                self.matches.append(played_stack[-1]) # still might have the last played card to start somthing
+                # still might have the last played card to start somthing
+                self.matches.append(played_stack[-1])
 
         return score
--- test_game_state.py
+++ test_game_state.py
@@ -4,8 +4,9 @@ from game_state import GameState
 import random
 from deck import Card, Rank, Suit, Deck
 
+
 class TestGameState(unittest.TestCase):
-        
+
     def setUp(self):
         """
         Creates the following deck
@@ -18,36 +19,51 @@ class TestGameState(unittest.TestCase):
          A♦, 6♥, 5♠, 4♥]
         """
         random.seed(123)
-        
+
     def test_deal_hand(self):
         # Given a normal game state
         gs = GameState()
         # When the deal happens
         gs.deal()
         # Then the first player gets the first 6 cards
-        expected_player1_hand = Deck.all_from_string(["Q♦", "J♣", "4♦", "10♣", "8♠", "K♠"])
+        expected_player1_hand = Deck.all_from_string(
+            ["Q♦", "J♣", "4♦", "10♣", "8♠", "K♠"])
         self.assertEqual(gs.player1.hand, expected_player1_hand)
         # and the second player get the second 6 cards
-        expected_player2_hand = Deck.all_from_string(["6♠", "10♠", "8♦", "Q♥", "5♣", "8♥",])
+        expected_player2_hand = Deck.all_from_string(
+            ["6♠", "10♠", "8♦", "Q♥", "5♣", "8♥", ])
         self.assertEqual(gs.player2.hand, expected_player2_hand)
 
     def test_prompt_crib(self):
         # Given a normal game state
-        firstCardLay = MagicMock(return_value=0) # players always lay away 0th card (first two in this case)
+        # players always lay away 0th card (first two in this case)
+        firstCardLay = MagicMock(return_value=0)
         gs = GameState(inputFn=firstCardLay)
         gs.deal()
         # When players lay their cards away...
         gs.make_crib()
         # Then the crib contains the laid off cards
-        self.assertListEqual(gs.crib, Deck.all_from_string(["Q♦", "J♣","6♠", "10♠"]))
-    
-    def test_prompt_with_invalid_index(self):
+        self.assertListEqual(
+            gs.crib, Deck.all_from_string(["Q♦", "J♣", "6♠", "10♠"]))
+
+    def test_select_card_with_invalid_index(self):
         # Given a player with a hand of 6 cards (indicies 0 to 5)
-        invalidCardLay = MagicMock(side_effect=[6, 0])
-        gs = GameState(inputFn=invalidCardLay)
+        invalid_card_lay = MagicMock(side_effect=["6", "0"])
+        gs = GameState(inputFn=invalid_card_lay)
         gs.deal()
         # When the player selects an index out of bound, and then the first card
         expected_card = gs.player1.hand[0]
-        card = gs.select_card(gs.player1)
+        card = gs.select_card(gs.player1.hand)
+        # Then the first card is returned
+        self.assertEqual(card, expected_card)
+
+    def test_select_card_with_non_int(self):
+        # Given a player with a hand of 6 cards
+        non_int_lay = MagicMock(side_effect=["non_int", "0"])
+        gs = GameState(inputFn=non_int_lay)
+        gs.deal()
+        # When the player provides a non integer instead of an index, and then the first card
+        expected_card = gs.player1.hand[0]
+        card = gs.select_card(gs.player1.hand)
         # Then the first card is returned
         self.assertEqual(card, expected_card)
